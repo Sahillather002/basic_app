e2399ba036604e3536f6d0771532d46f92e9f5ea
sahillather002 <sahillather002@gmail.com>
2023-01-30 08:33:30 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,17 @@
 import "./App.css";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import Main from "./components/Main";
-import Card from "./components/Card";
-import { Grid, Container, Box } from "@mui/material";
-
+import Home from "./pages/Home";
+import AddProduct from "./pages/AddProduct";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./components/Navbar";
 function App() {
-  const [posts, setPosts] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data } = await axios.get("/posts");
-      setPosts(data);
-    };
-    fetchData();
-  }, []);
   return (
     <>
-      <Navbar />
-      <Main />
-      <Container>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          {posts &&
-            posts.map((item) => {
-              return (
-                <Grid key={item.id} item xs={4}>
-                  <Card item={item} />
-                </Grid>
-              );
-            })}
-        </Grid>
-      </Container>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/addProduct" component={AddProduct} />
+        <Route path="*" component={Home} />
+      </Switch>
     </>
   );
 }
